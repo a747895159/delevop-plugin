37b5192f488a08e142ee4d1cecfeac01b27c29e0
zhoubin <zhoubin2@yonghui.cn>
2024-05-08 14:03:49 +0800

    [Util] block块解析

--- src/main/java/org/zb/plugin/runtest/H2MRun03.java
+++ src/main/java/org/zb/plugin/runtest/H2MRun03.java
@@ -1,11 +1,7 @@
 package org.zb.plugin.runtest;
 
 
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.tuple.MutablePair;
 import org.zb.plugin.html2md.HtmlHandlerUtil;
-
-import java.io.FileOutputStream;
 
 /**
  * 异步-selenium方式获取页面内容
@@ -23,25 +19,19 @@ public class H2MRun03 {
          * https://www.toutiao.com/article/6730619165612179979
          */
 
-        System.setProperty("webdriver.chrome.driver", "D:\\develop\\Tools\\chromedriver.exe");
-        MutablePair<String, String> convert = HtmlHandlerUtil.parseHtml("https://www.jianshu.com/p/0ff1b6a3da36", "");
-        String title = convert.getLeft();
-        String value = convert.getRight();
-        IOUtils.write(value, new FileOutputStream("D:\\data\\" + title + ".md"), "utf-8");
-        System.out.println(value);
+//        System.setProperty("webdriver.chrome.driver", "D:\\develop\\Tools\\chromedriver.exe");
+//        MutablePair<String, String> convert = HtmlHandlerUtil.parseHtml("https://www.jianshu.com/p/0ff1b6a3da36", "");
+//        String title = convert.getLeft();
+//        String value = convert.getRight();
+//        IOUtils.write(value, new FileOutputStream("D:\\data\\" + title + ".md"), "utf-8");
+//        System.out.println(value);
 
-//        parseHTML();
+        parseHTML();
     }
 
     private static void parseHTML(){
-        String html ="<html><body><blockquote>\n" +
-                "<ol>\n" +
-                "<li><p>producer 向<code>topic</code>发送消息，同一个<code>topic</code>下存在多个队列，<code>RocketMQ</code> 的生产者在默认发送消息时轮询选取其中一个队列进行发送，会导致消息分散到两个队列上</p></li>\n" +
-                "<li><p>broker 上的消息只有在同一个队列中消息才是顺序读取的</p></li>\n" +
-                "<li><p>消费组消费消息时，每一个consumer 单独消费broker 上的一个队列，一般情况下一个consumer 一个进程，不同进程消费不能保证消费的顺序性</p></li>\n" +
-                "<li><p>同一个进程下，通常会配置多个线程消费，多个线程消费的情况下没办法保证消费顺序</p></li>\n" +
-                "</ol>\n" +
-                "</blockquote></body></html>";
+        String html ="<p style=\"margin-bottom: 0px;padding-top: 8px;padding-bottom: 8px;letter-spacing: normal;text-align: left;caret-color: rgb(51, 51, 51);outline: 0px;background-color: rgb(255, 255, 255);font-size: 15px;color: rgb(74, 74, 74);font-family: system-ui, -apple-system, BlinkMacSystemFont, &quot;Helvetica Neue&quot;, &quot;PingFang SC&quot;, &quot;Hiragino Sans GB&quot;, &quot;Microsoft YaHei UI&quot;, &quot;Microsoft YaHei&quot;, Arial, sans-serif;line-height: 1.75em;\"><img class=\"rich_pages wxw-img\" data-ratio=\"0.42685185185185187\" data-src=\"https://mmbiz.qpic.cn/sz_mmbiz_jpg/kNkb0KtfticsJOw5YKtttF0CeibuIpzQLjpZQ8yCwHZma5yMnol885WKnnvdy1kDMr01SUicdp6ibKrls8t3gd0pjQ/640?wx_fmt=jpeg&amp;random=0.27984345744077976\" data-type=\"other\" data-w=\"1080\" style=\"vertical-align: inherit; width: 578px !important; height: auto !important; visibility: visible !important;\" data-original-style=\"vertical-align: inherit;width: 578px;height: auto !important;\" data-index=\"7\" src=\"https://mmbiz.qpic.cn/sz_mmbiz_jpg/kNkb0KtfticsJOw5YKtttF0CeibuIpzQLjpZQ8yCwHZma5yMnol885WKnnvdy1kDMr01SUicdp6ibKrls8t3gd0pjQ/640?wx_fmt=jpeg&amp;random=0.27984345744077976&amp;tp=webp&amp;wxfrom=5&amp;wx_lazy=1&amp;wx_co=1\" _width=\"578px\" crossorigin=\"anonymous\" alt=\"图片\" data-fail=\"0\"><span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, &quot;Hiragino Sans GB&quot;, &quot;Apple Color Emoji&quot;, &quot;Emoji Symbols Font&quot;, &quot;Segoe UI Symbol&quot;, Arial, sans-serif;font-size: 14px;\">以上流程基本遵循 发现问题 --&gt; 原理分析 --&gt; 解决问题 --&gt; 升华(编程规范)。</span></p>";
+
 
         String s = HtmlHandlerUtil.parseHtmlTag(html);
         System.out.println(s);
